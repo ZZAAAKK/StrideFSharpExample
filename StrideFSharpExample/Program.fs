37d9904e0984d6829089251a6b2fee20c1452e07
ZZAAAKK <48885763+ZZAAAKK@users.noreply.github.com>
2022-07-22 20:05:52 +0100

    Add project files.

---
+++ StrideFSharpExample/Program.fs
@@ -0,0 +1,17 @@
+﻿open Stride.Core.Mathematics
+open Stride.Engine
+open Stride.GameDefaults.ProceduralModels
+open Stride.GameDefaults.Extensions
+
+let game = new Game()
+
+let Start rootScene = 
+    game.SetupBase3DScene()
+    let entity = game.CreatePrimitive(PrimitiveModelType.Capsule)
+    entity.Transform.Position <- new Vector3(0f, 8f, 0f)
+    entity.Scene <- rootScene
+
+[<EntryPoint>]
+let main argv =
+    game.Run(start = Start)
+    0
